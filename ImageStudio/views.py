8cbbcd6a775829b08b556c0677611be31df71e33
iankurao <kuraoian@gmail.com>
2019-06-14 10:21:08 +0300

    create home view function

--- ImageStudio/views.py
+++ ImageStudio/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Create your views here.
+
+def home(request):
+    message='My Photos'
+    try:
+        images=Image.get_images()
+
+    except Exception as e:
+        raise  Http404()
+
+    locations=Location.get_location()
+    return render(request,"index.html",{'message':message})
