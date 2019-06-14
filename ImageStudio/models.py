8d16781f903407f912d16d183467d1d972bd1e38
iankurao <kuraoian@gmail.com>
2019-06-14 11:59:20 +0300

    create image,location and category models

--- ImageStudio/models.py
+++ ImageStudio/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Location(models.Model):
+    location=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.location
+
+
+
+
+class Category(models.Model):
+    Category=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.Category
+
+
+
+class Image(models.Model):
+    name=models.CharField(max_length=60)
+    decription=models.CharField(max_length=200)
+    locate=models.ForeignKey(Location)
+    categ=models.ForeignKey(Category)
+    image=models.ImageField(upload_to='galleria/',blank=True)
+    upload_time=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name 
+
+
+
+
