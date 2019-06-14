d50042c12d92ceedfc7406d552c80516df00267c
iankurao <kuraoian@gmail.com>
2019-06-14 12:12:08 +0300

    test save and delete methods for location and category models

--- ImageStudio/tests.py
+++ ImageStudio/tests.py
@@ -19,3 +19,52 @@ class LocationTest(TestCase):
         self.ian.delete_location()
         location=Location.objects.all()
         self.assertTrue(len(location)==0)
+
+class TestCategory(TestCase):
+    def setUp(self):
+        self.category=Category(Category="love")
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category,Category))
+
+    def test_save_category(self):
+        self.category.save_category()
+        category=Category.objects.all()
+        self.assertTrue(len(category)>0)
+
+
+class ImageTest(TestCase):
+        def setUp(self):
+
+            self.ian=Location(location="Nairobi");
+            self.ian.save_location()
+
+            self.new_categ=Category(Category="food")
+            self.new_categ.save_category()
+
+
+            self.new_image=Image(name="lizania",decription="Finger Lingering",image="",categ=self.new_categ,locate=self.ian)
+            self.new_image.save()
+
+
+        def test_save_image(self):
+            self.new_image.save_image()
+            saved=Image.objects.all()
+            self.assertTrue(len(saved)>0)
+
+        def test_delete_image(self):
+            self.new_image.delete_image()
+            saved=Image.objects.all()
+            self.assertTrue(len(saved)==0)
+
+        def test_test_image_id(self):
+            self.new_image.get_id()
+            id=Image.objects.all()
+            self.assertTrue(len(id)>0)
+
+
+        def TearDown(self):
+            Location.objects.all().delete()
+            Category.objects.all().delete()
+            Image.objects.all().delete()
